19cd55d169901b88918e9c2ae82631fa013d12f4
Aditya Jain <jaditya98@gmail.com>
2024-03-18 16:35:13 +0530

    fix bug

--- server/controllers/listing.controller.js
+++ server/controllers/listing.controller.js
@@ -2,7 +2,7 @@ import Listing from "../models/listing.model.js";
 import catchAsyncError from "../utils/catchAsyncError.js";
 
 export const createListing = catchAsyncError(async (req, res, next) => {
-  const listing = await Listing.create(req.body);
+  const listing = await Listing.create(req.body); // create method to store multiple documents
 
   return res.status(201).json(listing);
 });
